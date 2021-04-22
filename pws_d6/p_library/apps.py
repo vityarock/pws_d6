b1a092fdb08b4ee6ff0d29acb402d2fb1d883053
user <user@ivc1-m9.m9.maxidom.ru>
2021-04-22 21:35:57 +0300

    update project

--- pws_d6/p_library/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PLibraryConfig(AppConfig):
-    name = 'p_library'
--- pws_d6/p_library/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Book
-
-
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ('title', 'cover')
-        
